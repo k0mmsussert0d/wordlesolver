6e241d35c92ce3064ad0adcd0ab36f2b10f974e4
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2022-03-20 13:05:37 +0100

    Improved dictionary utility script

--- prepare_dictionary.sh
+++ prepare_dictionary.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-set -eax
+set -ea
 
-DICT_FILE="./wordlist_raw"
-OUT_FILE="./wordlist"
+DICT_FILE="$1"
+OUT_FILE="$2"
 awk 'length($0) == 5 { print $0 }' "$DICT_FILE" > "$OUT_FILE"
+
+INPUT_WC=$(wc -l "$DICT_FILE")
+OUTPUT_WC=$(wc -l "$OUT_FILE")
+
+echo "$INPUT_WC > $OUTPUT_WC"
